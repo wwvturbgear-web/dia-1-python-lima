02eb43d343b99f8f291afa56b1bfc38edb00d336
Willian <wwvturbgear@gmail.com>
2026-05-04 18:34:54 -0500

    Dìa 2: mi primer programa con input

---
+++ dìa2.py
@@ -0,0 +1,9 @@
+nombre = input("¿Còmo te llamas? ")
+ciudad = "lima"
+año = 2026
+
+print("hola" , nombre)
+print("Estàs programando desde" , ciudad)
+print("Estamos en el año" , año)
+print(nombre, "ya eres dev. Bienvenido al dìa 2 ")
+# no me deja guardar
