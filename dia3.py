dec5eaff0cead36e71244fb0e7987a45fe99cc3c
Willian <wwvturbgear@gmail.com>
2026-05-05 19:26:06 -0500

    dia 3: if/else desbloqueado

---
+++ dia3.py
@@ -0,0 +1,16 @@
+nombre = input("¿còmo te llmas, dev? ")
+edad = int(input("¿cuàntos años tienes?" ))
+
+print(f"Hola {nombre}, tienes {edad} años")
+
+if edad >= 18:
+    print("acceso nivel senior desbloqueado ")
+    print("Ya puedes hacer push a main sin miedo")
+
+else:
+    print("Nivel junior con futuro de CTO ")
+    print("Sigue practicando y rompes Github")
+
+if nombre == "Willian":
+    print("El profe de Lima detectado. Bienvenido")
+    
