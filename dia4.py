049b6fe7c859c2e16ce232f903e552f136a652e2
Willian <wwvturbgear@gmail.com>
2026-05-05 20:02:27 -0500

    dia 4: for loops desbloqueado - sin lag

---
+++ dia4.py
@@ -0,0 +1,12 @@
+print("== CONTADOR DRAKULA ACTIVADO ===")
+
+for numero in range(1, 6):
+    print(f"Vuelta {numero}: Willian codea sin lag")
+
+print("\n¿Cuàntos saludos quieres?")
+veces = int(input("Pon un numero: ")) 
+
+for i in range(veces):
+    print(f"Saludo #{i+1}: ¡Desde lima hasta la puna sin lag!")
+
+    
